866c5c3eedd332807f593c6a60288521400df20b
Sharoon <sharoon7171@gmail.com>
2026-09-18 02:46:25 +0500

    allow Prime r2 segment CDNs and require Referer

--- src/servers/prime.ts
+++ src/servers/prime.ts
@@ -2,10 +2,11 @@ import { type ServerProfile, vidcoreHeaders } from './types.js';
 
 export const prime: ServerProfile = {
   name: 'Prime',
-  hosts: ['moon.peakstorm.top', 'keenanchor.top'],
+  hosts: ['moon.peakstorm.top', 'keenanchor.top', 'northoak.top', 'lunarcabin.top', 'thunderpencil.site'],
   needsProxy: true,
   directPlayable: true,
-  refererRequired: false,
+  refererRequired: true,
   abrMaster: true,
+  segmentPathIncludes: ['/r2/cdn2/'],
   headers: { ...vidcoreHeaders },
 };
